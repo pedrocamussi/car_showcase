21e1fbda071fec5ad4c7ff06bd3ea7d4485a2e71
Pedro H. C. Carrasco <pedrocamussi@gmail.com>
2023-09-27 22:29:26 -0300

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Image from 'next/image';
+import { CustomButton } from '.';
+
+const Hero = () => {
+  const handleScroll = () => {
+    
+  }
+
+  return (
+    <div className='hero'>
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className='hero__title'>
+                Find, book, or rent a car — quickly and easily!
+            </h1>
+            <p className='hero__subtitle'>
+                Streamline your rental experience with our effortles booking process.
+            </p>
+
+            <CustomButton 
+              title="Explore Cars"
+              containerStyles="bg-primary-blue text-white rounded-full mt-10"
+              handleClick={handleScroll}
+            />
+            <div className="hero__image-container">
+              <div className="hero__image">
+
+              </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
